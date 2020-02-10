014d88d7727c82ea5557a89ba2dad909baad1543
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-02-10 18:12:17 -0400

    Este es el commit inicial

---
+++ Entidades/Articulos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiguelGondres_Parcial1.Entidades
+{
+    public class Articulos
+    {
+        public string Nombre;
+
+        public Articulos()
+        {
+            Nombre = string.Empty;
+        }
+    }
+}
